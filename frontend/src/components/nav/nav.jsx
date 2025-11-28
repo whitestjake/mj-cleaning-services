2cf068c8115ced087301495a617cc02d81bcd0e9
Jacob White <jakerobertwhite@gmail.com>
2025-11-28 09:27:22 -0500

    updated navbar
    added id's to navbar.jsx

--- frontend/src/components/nav/nav.jsx
+++ frontend/src/components/nav/nav.jsx
@@ -5,10 +5,10 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
 
     return (
-    <nav >
-      <h2>MJ</h2>
+    <nav id='navbar-container'>
+      <h2 id='logo'>MJ</h2>
 
-      <ul>
+      <ul id='navbar-links'>
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/login'>Login</Link></li>
         <li><Link to='/register'>Register</Link></li>
